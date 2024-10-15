fc2436b37cb059a29d618819a5bcc362c51a213d
Lesia Hlukhaniuk <olesia.hlukhaniuk@computools.com>
2024-10-15 21:09:09 +0300

    add user component

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { User } from './user.model';
+
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  // name = input.required<string>();
+  // select = output<string>();
+  @Input({ required: true }) selected!: boolean;
+  @Input({ required: true }) user!: User;
+  @Output() select = new EventEmitter<string>();
+
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+}
