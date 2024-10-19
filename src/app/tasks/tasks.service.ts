ab9002f0e2af32142f7ec80a15b623a5df45f42e
Lesia Hlukhaniuk <olesia.hlukhaniuk@computools.com>
2024-10-19 20:05:36 +0300

    store tasks in localStorage

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -31,6 +31,14 @@ export class TasksService {
     },
   ];
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTask(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
   }
@@ -43,9 +51,15 @@ export class TasksService {
       summary: taskData.summary,
       dueDate: taskData.date,
     });
+    this.saveTask();
   }
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveTask();
+  }
+
+  private saveTask() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
